3ae68190847a6d42b1a0fc1ead66a9263a9e6ba2
TobiAlbertino <tobialbertino@gmail.com>
2021-06-22 18:41:58 +0700

    data validation implemented

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
